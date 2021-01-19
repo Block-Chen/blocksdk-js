8cd6560c8ff7e64e40bbca2aa33a00d0361e7f89
blockchen-admin <61440698+blockchen-admin@users.noreply.github.com>
2021-01-19 11:18:29 +0900

    Update dash.js

--- src/dash.js
+++ src/dash.js
@@ -3,202 +3,195 @@ var BlockSDK = BlockSDK || {};
 
 const util = require('util');
 
-	BlockSDK.Dash = function(api_token = '') {
+	BlockSDK.Dash = function(api_token = ''){
 
-			Base.apply(this, arguments);
+		Base.apply(this, arguments);
 
-			this.getBlockChain = function () {
-			return this.request("GET","/dash/block");}
+		this.api_token = api_token;
 
-			this.getBlock = function(request={}){
 
-			if ( typeof request['rawtx'] == 'undefined') request['rawtx']=false;
+		this.getBlockChain = function (){
+			return this.request("GET","/dash/info");
+		}
 
-			if ( typeof request['offset'] == 'undefined') request['offset']=0;
+		this.getBlock = function (request = {}){
+			if (typeof request['limit'] == 'undefined') request['limit'] = 10;
+			if (typeof request['offset'] == 'undefined') request['offset'] = 0;
+			if (typeof request['rawtx'] == 'undefined') request['rawtx'] = false;
 
-			if ( typeof request['limit'] == 'undefined') request['limit']=10;
 
-			return this.request("GET",`/dash/block/${request['block']}`,{
-				"rawtx" : request['rawtx'],
-				"offset" : request['offset'],
-				"limit" : request['limit']
+			return this.request("GET",`/dash/blocks/${request['block']}`,{
+				"rawtx": request['rawtx'],
+				"offset": request['offset'],
+				"limit": request['limit']
 			});
 		}
 
-		 this.getMemPool = function(request={}){
-
-			if ( typeof request['rawtx'] == 'undefined' ) request['rawtx']=false;
-
-			if ( typeof request['offset'] == 'undefined') request['offset']=0;
-
-			if ( typeof request['limit'] == 'undefined') request['limit']=10;
+		this.getMemPool = function (request = {}){
+			if (typeof request['limit'] == 'undefined') request['limit'] = 10;
+			if (typeof request['offset'] == 'undefined') request['offset'] = 0;
+			if (typeof request['rawtx'] == 'undefined') request['rawtx'] = false;
 
 			return this.request("GET","/dash/mempool",{
-				"rawtx" : request['rawtx'],
-				"offset" : request['offset'],
-				"limit" : request['limit']
+				"rawtx": request['rawtx'],
+				"offset": request['offset'],
+				"limit": request['limit']
 			});
 		}
 
-		 this.getAddressInfo = function(request={}){
+		this.getAddressInfo = function (request = {}){
+			if (typeof request['limit'] == 'undefined') request['limit'] = 10;
+			if (typeof request['offset'] == 'undefined') request['offset'] = 0;
+			if (typeof request['rawtx'] == 'undefined') request['rawtx'] = null;
 
-			if ( typeof request['rawtx'] == 'undefined') request['rawtx']=null;
-
-			if ( typeof request['offset'] == 'undefined') request['offset']=0;
-
-			if ( typeof request['limit'] == 'undefined') request['limit']=10;
-
-
-			return this.request("GET",`/dash/address/${request['address']}`,{
-				"rawtx" : request['rawtx'],
-				"offset" : request['offset'],
-				"limit" : request['limit']
+			return this.request("GET",`/dash/addresses/${request['address']}`,{
+				"reverse":request['reverse'],
+				"rawtx":request['rawtx'],
+				"offset":request['offset'],
+				"limit":request['limit']
 			});
 		}
 
-		 this.getAddressBalance = function(request={}){
-			return this.request("GET",`/dash/address/${request['address']}/balance`);
+
+		this.getAddressBalance = function (request = {}){
+
+			return this.request("GET",`/dash/addresses/${request['address']}/balance`);
 		}
 
-		 this.listWallet = function(request = null){
-			if ( typeof request['offset'] == 'undefined') request['offset']=0;
 
-			if ( typeof request['limit'] == 'undefined') request['limit']=10;
+		this.getWallets = function (request = {}){
+			if (typeof request['limit'] == 'undefined') request['limit'] = 10;
+			if (typeof request['offset'] == 'undefined') request['offset'] = 0;
 
-			return this.request("GET","/dash/wallet",{
-				"offset" : request['offset'],
-				"limit" : request['limit']
+			return this.request("GET","/dash/wallets",{
+				"offset":request['offset'],
+				"limit":request['limit']
+			});
+		}
+		
+		
+		this.getWallet = function (request = {}){
+
+			return this.request("GET",`/dash/wallets/${request['wallet_id']}`);
+		}
+
+		this.createHdWallet = function (request = {}){
+			if (typeof request['name'] == 'undefined') request['name'] = null;
+
+			return this.request("POST","/dash/wallets/hd",{
+				"name": request['name']
 			});
 		}
 
-		this.createWallet = function(request = null){
-
-			if ( typeof request['name'] == 'undefined') request['name']=null;
-
-			return this.request("POST","/dash/wallet",{
-				"name" : request['name']
-			});
-		}
-
-		this.loadWallet = function (request={}){
+		this.loadWallet = function (request = {}){
 			return this.request("POST",`/dash/wallet/${request['wallet_id']}/load`,{
-				"seed_wif" : request['seed_wif'],
-				"password" : request['password']
+				"wif": request['wif'],
+				"password": request['password']
 			});
 		}
 
-		this.unLoadWallet = function (request={}){
-
-			return this.request("POST",`/dash/wallet/${request['wallet_id']}/unload`);
+		this.unloadWallets = function (request = {}){
+			return this.request("POST",`/dash/wallets/${request['wallet_id']}/unload`);
 		}
 
-		this.listWalletAddress = function (request={}){
-			if ( typeof request['address'] == 'undefined') request['address']=null;
 
-			if ( typeof request['hdkeypath'] == 'undefined') request['hdkeypath']=null;
+		this.getWalletAddresses = function (request = {}){
+			if (typeof request['address'] == 'undefined') request['address'] = null;
+			if (typeof request['hdkeypath'] == 'undefined') request['hdkeypath'] = null;
 
-			if ( typeof request['offset'] == 'undefined') request['offset']=0;
+			if (typeof request['limit'] == 'undefined') request['limit'] = 10;
+			if (typeof request['offset'] == 'undefined') request['offset'] = 0;
 
-			if ( typeof request['limit'] == 'undefined') request['limit']=10;
-
-			return this.request("GET",`/dash/wallet/${request['wallet_id']}/address`,{
-				"address" : request['address'],
-				"hdkeypath" : request['hdkeypath'],
-				"offset" : request['offset'],
-				"limit" : request['limit']
+			return this.request("GET",`/dash/wallets/${request['wallet_id']}/addresses`,{
+				"address": request['address'],
+				"hdkeypath": request['hdkeypath'],
+				"offset": request['offset'],
+				"limit": request['limit']
 			});
 		}
 
-		this.createWalletAddress = function (request={}){
 
-			if ( typeof request['seed_wif'] == 'undefined') request['seed_wif']=null;
+		this.createWalletAddress = function (request = {}){
+			if (typeof request['wif'] == 'undefined') request['wif'] = null;
+			if (typeof request['password'] == 'undefined') request['password'] = null;
 
-			if ( typeof request['password'] == 'undefined') request['password']=null;
-
-			return this.request("POST",`/dash/wallet/${request['wallet_id']}/address`,{
-				"seed_wif" : request['seed_wif'],
-				"password" : request['password']
+			return this.request("POST",`/dash/wallets/${request['wallet_id']}/addresses`,{
+				"wif": request['wif'],
+				"password": request['password']
 			});
 		}
 
-		this.getWalletBalance = function (request={}){
+		this.getWalletBalance = function (request = {}){
 
-			return this.request("GET",`/dash/wallet/${request['wallet_id']}/balance`);
+			return this.request("GET",`/dash/wallets/${request['wallet_id']}/balance`);
 		}
 
-		this.getWalletTransaction = function (request={}){
 
-			if ( typeof request['category'] == 'undefined') request['category']='all';
+		this.getWalletTransactions = function (request = {}){
 
-			if ( typeof request['order'] == 'undefined' ) request['order']='desc';
+			if (typeof request['type'] == 'undefined') request['type'] = 'all';
+			if (typeof request['order'] == 'undefined') request['order'] = 'desc';
 
-			if ( typeof request['offset']== 'undefined') request['offset']=0;
+			if (typeof request['limit'] == 'undefined') request['limit'] = 10;
+			if (typeof request['offset'] == 'undefined') request['offset'] = 0;
 
-			if ( typeof request['limit'] == 'undefined') request['limit']=10;
-
-			return this.request("GET",`/dash/wallet/${request['wallet_id']}/transaction`,{
-				"category" : request['category'],
-				"order" : request['order'],
-				"offset" : request['offset'],
-				"limit" : request['limit']
+			return this.request("GET",`/dash/wallets/${request['wallet_id']}/transaction`,{
+				"type": request['type'],
+				"order": request['order'],
+				"offset": request['offset'],
+				"limit": request['limit']
 			});
 		}
 
-		this.sendToAddress = function (request={}){
+		this.sendToAddress = function (request = {}){
 
-			if ( typeof request['kbfee'] == 'undefined')
-			{
-				blockChain = this.getBlockChain();
+			if(typeof request['kbfee'] == 'undefined'){
+				var blockChain = this.getBlockChain();
 				if(typeof blockChain['medium_fee_per_kb'] !== 'undefined'){
 				request['kbfee'] = blockChain['medium_fee_per_kb'];
 			}
 			}
 
-			if ( typeof request['seed_wif'] == 'undefined') request['seed_wif']=null;
-
-			if ( typeof request['password'] == 'undefined') request['password']=null;
-
+			if (typeof request['wif'] == 'undefined') request['wif'] = null;
+			if (typeof request['password'] == 'undefined') request['password'] = null;
 			if (typeof request['subtractfeefromamount'] == 'undefined') request['subtractfeefromamount'] = false;
 
-			return this.request("POST",`/dash/wallet/${request['wallet_id']}/sendtoaddress`,{
-				"address" : request['address'],
-				"amount" : request['amount'],
-				"seed_wif" : request['seed_wif'],
-				"password" : request['password'],
-				"kbfee" : request['kbfee'],
+			return this.request("POST",`/dash/wallets/${request['wallet_id']}/sendtoaddress`,{
+				"address": request['address'],
+				"amount": request['amount'],
+				"wif": request['wif'],
+				"password": request['password'],
+				"kbfee": request['kbfee'],
 				"subtractfeefromamount" : request['subtractfeefromamount']
 			});
 		}
 
-		this.sendMany = function (request={}){
+		this.sendMany = function (request = {}){
 
-			if ( typeof request['seed_wif'] == 'undefined') request['seed_wif']=null;
-
-			if ( typeof request['password'] !== 'undefined') request['password']=null;
-
+			if (typeof request['wif'] == 'undefined') request['wif'] = null;
+			if (typeof request['password'] == 'undefined') request['password'] = null;
 			if (typeof request['subtractfeefromamount'] == 'undefined') request['subtractfeefromamount'] = false;
 
-			return this.request("POST",`/dash/wallet/{$request['wallet_id']}/sendmany`,{
+			return this.request("POST",`/dash/wallets/${request['wallet_id']}/sendmany`,{
 				"to" : request['to'],
-				"seed_wif" : request['seed_wif'],
+				"wif" : request['wif'],
 				"password" : request['password'],
 				"subtractfeefromamount" : request['subtractfeefromamount']
 			});
 		}
 
-	this.sendTransaction = function (request={}){
+		this.sendTransaction = function (request = {}){
 
-			return this.request("POST","/dash/transaction",{
-				"sign_hex" : request['sign_hex']
+			return this.request("POST","/dash/transactions/send",{
+				"hex" : request['hex']
 			});
 		}
 
-	this.getTransaction = function (request={}){
+		this.getTransaction = function (request = {}){
 
-			return this.request("GET",`/dash/transaction/${request['hash']}`);
+			return this.request("GET",`/dash/transactions/${request['hash']}`);
 		}
-}
-
+	};
 
 
 util.inherits(BlockSDK.Dash, Base);
